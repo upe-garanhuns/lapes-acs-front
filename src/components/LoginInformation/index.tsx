6ed5876dabd09487986e65e467abdb65930a8b9d
Lucas Monteiro <77646992+lucas752@users.noreply.github.com>
2023-06-28 19:39:15 -0300

    feat: adicionando parte de informações da página de login

---
+++ src/components/LoginInformation/index.tsx
@@ -0,0 +1,30 @@
+'use client';
+
+import * as S from './styles';
+
+export default function LoginInformation() {
+  return (
+    <S.Container>
+      <S.TextContainer>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Egestas erat
+        imperdiet sed euismod. Amet venenatis urna cursus eget nunc scelerisque
+        viverra mauris. Donec adipiscing tristique risus nec feugiat in. In
+        mollis nunc sed id semper risus in. Purus in mollis nunc sed id semper
+        risus. Ornare suspendisse sed nisi lacus sed viverra. Velit sed
+        ullamcorper morbi tincidunt ornare massa eget. Cursus risus at ultrices
+        mi tempus imperdiet nulla. Id velit ut tortor pretium viverra
+        suspendisse potenti nullam ac. Congue mauris rhoncus aenean vel elit
+        scelerisque mauris pellentesque. Enim nulla aliquet porttitor lacus
+        luctus accumsan tortor posuere ac. Id aliquet lectus proin nibh nisl.
+        Rutrum tellus pellentesque eu tincidunt tortor aliquam nulla facilisi
+        cras. Est ante in nibh mauris cursus mattis molestie a iaculis.
+        Convallis aenean et tortor at risus viverra adipiscing at in. Sit amet
+        risus nullam eget felis eget nunc lobortis. Dictum at tempor commodo
+        ullamcorper a lacus vestibulum sed arcu. Molestie nunc non blandit
+        massa. Massa sapien faucibus et molestie ac feugiat sed lectus
+        vestibulum.
+      </S.TextContainer>
+    </S.Container>
+  );
+}
