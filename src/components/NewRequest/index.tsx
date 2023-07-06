2b7910f5bcde3ae392a55ae9c3b31e3bd3567f30
Jamuelton <78917005+Jamuelton@users.noreply.github.com>
2023-07-06 19:40:17 -0300

    chore: tentando consertar erro de merge

---
+++ src/components/NewRequest/index.tsx
@@ -0,0 +1,87 @@
+import React, { useState } from 'react';
+
+import * as S from './styles';
+
+import { XCircle, FileText, FilePlus } from '@phosphor-icons/react';
+
+type ComponentProps = {
+  cancelRequest: () => void;
+};
+
+export const NewRequest = ({ cancelRequest }: ComponentProps) => {
+  const [uploadedFiles, setUploadedFiles] = useState<File[]>([]);
+
+  const handleFileUpload = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const files = event.target.files;
+    if (files) {
+      setUploadedFiles((prevFiles) => [...prevFiles, ...Array.from(files)]);
+    }
+  };
+
+  const handleRemoveFile = (index: number) => {
+    setUploadedFiles((prevFiles) => {
+      const updatedFiles = [...prevFiles];
+      updatedFiles.splice(index, 1);
+      return updatedFiles;
+    });
+  };
+
+  const handleNext = () => {
+    // Lógica para avançar para a próxima etapa
+  };
+
+  const handleDragOver = (event: React.DragEvent<HTMLDivElement>) => {
+    event.preventDefault();
+  };
+
+  const handleDrop = (event: React.DragEvent<HTMLDivElement>) => {
+    event.preventDefault();
+    const files = event.dataTransfer.files;
+    if (files && files.length > 0) {
+      setUploadedFiles((prevFiles) => [...prevFiles, ...Array.from(files)]);
+    }
+  };
+
+  return (
+    <S.Container onDragOver={handleDragOver} onDrop={handleDrop}>
+      <S.Title>Etapa 1 de 3 - Anexar certificados</S.Title>
+      <S.Subtitle>
+        <FileText
+          size={24}
+          color="#1C3C78
+            "
+        />
+        Barema
+      </S.Subtitle>
+      <S.FileInputContainer>
+        <S.FileInputLabel htmlFor="selecao-arquivo">
+          Arraste aqui o(s) arquivo(s) desejado(s)
+          <FilePlus size={41} color="#4A4747" />
+        </S.FileInputLabel>
+        <S.FileInput
+          id="selecao-arquivo"
+          type="file"
+          onChange={handleFileUpload}
+          multiple
+        />
+      </S.FileInputContainer>
+      <S.FileListContainer>
+        <S.FileList>
+          {uploadedFiles.map((file, index) => (
+            <S.FileItem key={index}>
+              <S.FileName>{file.name}</S.FileName>
+              <S.FileRemoveButton onClick={() => handleRemoveFile(index)}>
+                <XCircle color="#FF0000" size={20} />
+              </S.FileRemoveButton>
+            </S.FileItem>
+          ))}
+        </S.FileList>
+      </S.FileListContainer>
+
+      <S.ButtonsContainer>
+        <S.CancelButton onClick={cancelRequest}>Cancelar</S.CancelButton>
+        <S.NextButton onClick={handleNext}>Próximo</S.NextButton>
+      </S.ButtonsContainer>
+    </S.Container>
+  );
+};
