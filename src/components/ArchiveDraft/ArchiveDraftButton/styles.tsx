ed0784c549bd71f7abbbbadc309e9dcf505fddd3
MikhaelVinicius <mickaelsoel@gmail.com>
2023-07-24 20:22:09 -0300

    feat: adicionado o componente ArchiveDraft

---
+++ src/components/ArchiveDraft/ArchiveDraftButton/styles.tsx
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const ArchiveButton = styled.button`
+  padding: 10px;
+  width: 10rem;
+  height: 2.5rem;
+  border-radius: 15px;
+  background-color: #253555;
+  color: white;
+  font-weight: Bold;
+  border-radius: 20px;
+  box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
+  margin: 0.6rem;
+`;
