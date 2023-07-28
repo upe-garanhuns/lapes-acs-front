56f2286fa2b13be9a2fbe9b0979620fbd6becbab
Jamuelton <78917005+Jamuelton@users.noreply.github.com>
2023-07-28 15:23:17 -0300

    chore: criando parte inicial da visualiza;'aodo certificado

---
+++ src/app/visualizar-certificado/page.tsx
@@ -0,0 +1,17 @@
+'use client';
+
+import * as S from './styles';
+
+export default function SignIn() {
+  return (
+    <S.Container>
+      <S.ContentDiv>
+        <S.PrincipalDiv>
+          <S.Div>conteudo</S.Div>
+          <S.Div>previewe</S.Div>
+        </S.PrincipalDiv>
+        <S.Div>side</S.Div>
+      </S.ContentDiv>
+    </S.Container>
+  );
+}
---
+++ src/app/visualizar-certificado/styles.tsx
@@ -0,0 +1,24 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: row;
+  width: 100%;
+  height: 100vh;
+  background: #f1efef;
+`;
+
+export const ContentDiv = styled.div`
+  width: 100%;
+  margin-left: 100px;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+`;
+export const PrincipalDiv = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+export const Div = styled.div``;
