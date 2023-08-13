6f80f8ceabced2a9c4cc87317306160126825567
Camila Claudino <camila.claudinoo123@gmail.com>
2023-08-13 18:57:54 -0300

    feat: criação do footer

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,23 @@
+import Image from 'next/image';
+
+import * as S from './styles';
+
+export default function Footer() {
+  return (
+    <S.Container>
+      <S.ImageContainer>
+        <a href="http://www.upe.br/garanhuns/">
+          <Image width={110} height={70} src={'/logo-upe.png'} alt="Upe" />
+        </a>
+      </S.ImageContainer>
+      <div>
+        <S.BoldParagraph>Universidade de Pernambuco</S.BoldParagraph>
+        <S.BoldParagraph>Campus Garanhuns</S.BoldParagraph>
+        <S.Paragraph>
+          R. Cap. Pedro Rodrigues - São José, Garanhuns - PE
+        </S.Paragraph>
+        <S.Paragraph>CEP: 55294-902 | Fone: (87) 3761-8227</S.Paragraph>
+      </div>
+    </S.Container>
+  );
+}
