da7ae2d640baea38b66b0f37189508128416fc2a
Jamuelton <jamueltonangelim@gmail.com>
2023-08-28 23:34:08 -0300

    fix: finalizando componente repoosnivo do minhas horas

--- src/app/home/components/HourCount/index.tsx
+++ src/app/home/components/HourCount/index.tsx
@@ -36,10 +36,10 @@ const HourCount: React.FC<ComponentProps> = ({
     <S.Component>
       <S.Container>
         <S.MinhasHorasString>Minhas Horas: </S.MinhasHorasString>
-        <S.Bar>Ges: {gesHours}h</S.Bar>
-        <S.Bar>Ext: {extHours}h</S.Bar>
-        <S.Bar>Pes: {pesHours}h</S.Bar>
-        <S.Bar>Ens: {ensHours}h</S.Bar>
+        <S.Bar color="#00798c">Ges: {gesHours}h</S.Bar>
+        <S.Bar color="#ec2026">Ext: {extHours}h</S.Bar>
+        <S.Bar color="#d8f793">Pes: {pesHours}h</S.Bar>
+        <S.Bar color="#3b0086">Ens: {ensHours}h</S.Bar>
       </S.Container>
       <S.TotalBarComponent>
         <S.TotalHoursDatas>
@@ -47,10 +47,10 @@ const HourCount: React.FC<ComponentProps> = ({
           <S.TotalBarLine>{totalHours}/180</S.TotalBarLine>
         </S.TotalHoursDatas>
         <S.TotalBarBackgroundColor>
-          <S.TotalBarGes width={gesWidth} />
-          <S.TotalBarExt width={extWidth} />
-          <S.TotalBarPes width={pesWidth} />
-          <S.TotalBarEns width={ensWidth} />
+          <S.TotalBar width={gesWidth} color="#00798c" />
+          <S.TotalBar width={extWidth} color="#ec2026" />
+          <S.TotalBar width={pesWidth} color="#d8f793" />
+          <S.TotalBar width={ensWidth} color="#3b0086" />
         </S.TotalBarBackgroundColor>
       </S.TotalBarComponent>
     </S.Component>
--- src/app/home/components/HourCount/styles.ts
+++ src/app/home/components/HourCount/styles.ts
@@ -1,36 +1,42 @@
 import styled from 'styled-components';
 
+interface TotalBarProps {
+  width: string;
+  color: string;
+}
+
+interface colorType {
+  color: string;
+}
+
 export const Component = styled.div`
-  border-radius: 8px;
-  padding: 10px;
   display: flex;
   flex-direction: column;
-
-  flex-wrap: nowrap;
+  justify-content: center;
+  align-items: flex-start;
+  padding: 10px;
   @media screen and (max-width: 767px) {
-    flex-direction: row;
-    gap: 10;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
   }
 
   @media screen and (min-width: 768px) and (max-width: 1023px) {
   }
 `;
 
-export const Name = styled.p`
-  font-family: 'Montserrat', sans-serif;
-  font-weight: bold;
-  font-size: 32px;
-  color: #1c3c78;
-`;
+export const Container = styled.div`
+  display: flex;
+  flex-direction: row;
 
-export const Hr = styled.hr`
-  border: 1px solid #1c3c78;
-  margin: 20px 0;
-  color: #1c3c78;
-`;
+  @media screen and (max-width: 767px) {
+    flex-direction: column;
+    align-items: center;
+    gap: 20;
+  }
 
-export const SubComponent = styled.div`
-  text-align: match-parent;
+  @media screen and (min-width: 768px) and (max-width: 1023px) {
+  }
 `;
 
 export const MinhasHorasString = styled.span`
@@ -41,89 +47,63 @@ export const MinhasHorasString = styled.span`
   font-weight: bold;
   font-size: 25px;
   white-space: nowrap;
-`;
-
-export const Container = styled.div`
-  display: flex;
-  flex-wrap: nowrap;
-
   @media screen and (max-width: 767px) {
-    flex-direction: column;
-    align-items: center;
-    gap: 10;
+    font-size: 1em;
   }
 
   @media screen and (min-width: 768px) and (max-width: 1023px) {
   }
 `;
 
-export const Bar = styled.p`
+export const Bar = styled.p<colorType>`
   display: inline-block;
   margin-right: 10%;
   border-bottom: 4px solid;
-  color: #00798c;
+  color: ${(props) => props.color};
   font-family: 'Montserrat', sans-serif;
   font-weight: regular;
   white-space: nowrap;
   font-size: 15px;
   border-radius: 3px 3px 3px 3px;
   @media screen and (max-width: 767px) {
-    width: 40%;
+    width: 50%;
+    padding: 0.2em;
   }
 
   @media screen and (min-width: 768px) and (max-width: 1023px) {
   }
-`;
-//color: #ec2026;
-//color: #d8f793;
-//color: #3b0086;
-
-export const TotalHoursDatas = styled.div`
-  display: flex;
-  align-items: center;
-  @media screen and (max-width: 767px) {
-    flex-direction: column;
-    align-items: center;
-    gap: 10;
-  }
-
-  @media screen and (min-width: 768px) and (max-width: 1023px) {
-  }
-`;
-
-export const TotalString = styled.p`
-  margin-left: 40px;
 `;
 
 export const TotalBarComponent = styled.div`
   margin: 5px;
-  border: 2px solid #d9d9d9;
   border-radius: 25px;
   background: #d9d9d9;
-`;
-
-export const TotalBarBorde = styled.p`
-  border: 0px;
-  margin-left: 4%;
-  margin-right: 0%;
-  margin-bottom: 0px;
-  border-top: 25px solid;
-  max-height: 50%;
-  display: flex;
-  align-items: center;
+  padding: 0.5em;
+  width: 75vw;
   @media screen and (max-width: 767px) {
-    flex-direction: column;
-    align-items: center;
-    gap: 10;
   }
 
   @media screen and (min-width: 768px) and (max-width: 1023px) {
   }
 `;
 
-export const TotalBarLine = styled.span`
-  margin-left: 75%;
+export const TotalHoursDatas = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  @media screen and (max-width: 767px) {
+    justify-content: space-around;
+    align-items: center;
+  }
+
+  @media screen and (min-width: 768px) and (max-width: 1023px) {
+  }
 `;
+
+export const TotalString = styled.p``;
+
+export const TotalBarLine = styled.span``;
 
 export const TotalBarBackgroundColor = styled.div`
   display: flex;
@@ -132,42 +112,17 @@ export const TotalBarBackgroundColor = styled.div`
   border-radius: 10px;
   display: flex;
   margin-bottom: 15px;
-  max-width: 95%;
-  margin-left: 35px;
+  flex-direction: row;
+  justify-content: center;
   @media screen and (max-width: 767px) {
-    flex-direction: column;
-    align-items: center;
-    gap: 10;
   }
 
   @media screen and (min-width: 768px) and (max-width: 1023px) {
   }
 `;
 
-interface TotalBarProps {
-  width: string;
-}
-
-export const TotalBarGes = styled.div<TotalBarProps>`
-  background-color: #00798c;
+export const TotalBar = styled.div<TotalBarProps>`
+  background-color: ${(props) => props.color};
   width: ${(props) => props.width};
   border-radius: 10px 0 0 10px;
-`;
-
-export const TotalBarExt = styled.div<TotalBarProps>`
-  background-color: #ec2026;
-  width: ${(props) => props.width};
-  border-radius: 0px 0 0 0px;
-`;
-
-export const TotalBarPes = styled.div<TotalBarProps>`
-  background-color: #d8f793;
-  width: ${(props) => props.width};
-  border-radius: 0px 0 0 0px;
-`;
-
-export const TotalBarEns = styled.div<TotalBarProps>`
-  background-color: #3b0086;
-  width: ${(props) => props.width};
-  border-radius: 0px 10px 10px 0px;
 `;
