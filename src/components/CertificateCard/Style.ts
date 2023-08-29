81414e68a84901df4e8a9ccd9ebcd26487cffd0f
Jamuelton <jamueltonangelim@gmail.com>
2023-08-29 00:15:33 -0300

    chore: comecando estrutura basica do componete de confirmacao

---
+++ src/components/CertificateCard/Style.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const Container = styled.div``;
+
+export const Content = styled.div``;
+
+export const Title = styled.p``;
+
+export const TextContent = styled.p``;
