683be7953c984272caf001def8cedf7ba900bb70
Jamuelton <jamueltonangelim@gmail.com>
2023-09-03 16:12:51 -0300

    feat: termiando compoente de paginacao

--- src/components/PaginationComp/styles.ts
+++ src/components/PaginationComp/styles.ts
@@ -1,1 +1,24 @@
+import { CaretCircleLeft, CaretCircleRight } from '@phosphor-icons/react';
 import styled from 'styled-components';
+
+export const LeftArrow = styled(CaretCircleLeft)`
+  cursor: pointer;
+`;
+export const RightArrow = styled(CaretCircleRight)`
+  cursor: pointer;
+`;
+
+export const PaginationDiv = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: 8px;
+  justify-content: flex-end;
+  align-items: center;
+`;
+
+export const CurrentPageNumber = styled.div``;
+export const PageNumber = styled.p`
+  font-size: 1em;
+  color: #5555ff;
+`;
+export const Div = styled.div``;
