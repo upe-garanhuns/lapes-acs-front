fe5e261beb4e134d4b1ff163343c64f7e5373020
Jamuelton <jamueltonangelim@gmail.com>
2023-09-10 11:43:36 -0300

    chore: criando estrutura dos componentes do modal de esquecer senha

---
+++ src/app/signin/components/forgetPassword/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import * as S from './styles';
+
+export const ForgetPassForm = () => {
+  return (
+    <div>
+      {' '}
+      <div></div>{' '}
+    </div>
+  );
+};
